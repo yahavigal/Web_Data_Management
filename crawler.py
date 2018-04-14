aeb848b99bb15b2391f13a30f7720dbe7642c2fe
yavigal <yahavigal@gmail.com>
2018-04-14 18:18:51 +0300

    working

--- crawler.py
+++ crawler.py
@@ -1,43 +1,56 @@
 import requests
 import lxml.html
 import networkx as nx
-import matplotlib.pyplot as plt
 
-DEBUG = True
+DEBUG = False
 MAX_DEPTH = 3
 MAX_LINKS = 10
 
-robot_url = "https://en.wikipedia.org/wiki/Robot"
+kiril_url = "/wiki/Kirill_Nababkin"
 prefix = "https://en.wikipedia.org"
 
+
 def crawl_rec(url, g, depth, visited):
-    if depth > MAX_DEPTH:
+    if depth > MAX_DEPTH or url in visited:
         return
     visited.add(url)
-    urls = []
-    r = requests.get(url)
+    urls = set()
+    r = requests.get(prefix + url)
     doc = lxml.html.fromstring(r.content)
-    i = 0
     if DEBUG:
         print("\n")
         print(url)
-    for t in doc.xpath("//a[contains(@href,'/wiki/') and not(contains(@href,':'))]/@href"):
+    for node in doc.xpath("//a[contains(@href,'/wiki/') and not(contains(@href,':'))]/@href"):
         if DEBUG:
-            print("---" + str(t))
-        node = prefix + t
-        urls.append(node)
-        g.add_node(node)
-        g.add_edge(url, node)
-        i += 1
-        if i == MAX_LINKS:
+            print("---" + str(node))
+
+        if node in urls:
+            continue
+
+        urls.add(node)
+
+        if depth == MAX_DEPTH:
+            if node in visited:
+                g.add_edge(url, node)
+        else:
+            if not g.has_node(node):
+                g.add_node(node)
+            g.add_edge(url, node)
+
+        if len(urls) == MAX_LINKS:
             break
+
     depth += 1
-    if len(urls) > 0:
-        for link in urls:
-            if link not in visited:
-                crawl_rec(link, g, depth, visited)
+    for link in urls:
+        crawl_rec(link, g, depth, visited)
+
 
 def crawl():              
     graph = nx.DiGraph()
-    graph.add_node(robot_url)
-    return crawl_rec(robot_url, graph, 0, set())
+    graph.add_node(kiril_url)
+    crawl_rec(kiril_url, graph, 0, set())
+    for node in graph.nodes():
+        print(node + " = {" + ", ".join(graph[node]) + "}")
+    return graph
+
+
--- pagerank.py
+++ pagerank.py
@@ -1,35 +1,32 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import crawler
-    
+
 def PageRank(G):
     N = G.number_of_nodes()
     d = 0.3
     iters = 60
-    
+
     if (N == 0):
         return
-    
+
     result = {node: [1/N, 1/N] for node in G.nodes()}
 
     for node in G.nodes():
         if G.out_degree(node) == 0:
             G.add_edge(node, node)
-    
+
     for i in range(iters):
         for node in G.nodes():
             result[node][i%2] = d*(1/N) + (1-d)*sum([result[prev][(i-1)%2]/G.out_degree(prev) for prev in G.predecessors(node)])
-            
-            
+
+
     return {k: v[0] for k,v in result.items()}
 
 
 G = crawler.crawl()
+print('\n')
 result = PageRank(G)
 sorted_result =  sorted(result.items(), key=lambda x: x[1], reverse=True)
 for url, rank in sorted_result:
     print(str(url)  + ": " + str(rank))
-
-    
-nx.draw(G, with_labels=True, node_color="skyblue", font_size=30, node_list=result.keys(), node_size=[v*30000 for v in result.values()])
-plt.show()
